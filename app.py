c2e2d81d61dc775b6d57835b491c542dffc85550
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2024-05-17 22:08:46 -0300

    Initial analysis: Includes chargeback statistics and calculates average transaction values

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-csv_file_path = 'spreadsheet/transactional-sample.csv'
+df = pd.read_csv('spreadsheet/transactional-sample.csv', delimiter=';')
 
-df = pd.read_csv(csv_file_path, engine='python')
+total_transactions = len(df)
+print("Total de Transações:", total_transactions)
 
-print(df.head())
+# Converter os valores da coluna 'has_cbk' para minúsculas, se necessário
+if df['has_cbk'].dtype == bool:
+    df['has_cbk'] = df['has_cbk'].astype(str).str.lower()
+
+# Contar o número de transações com e sem chargeback
+chargeback_counts = df['has_cbk'].value_counts()
+
+# Exibir a contagem de transações com e sem chargeback
+print("Contagem de Transações com e sem Chargeback:")
+print(chargeback_counts.to_string())
+
+# Verificar se há transações com chargeback
+if 'true' in chargeback_counts.index:
+    # Calcular o percentual de transações com chargeback
+    total_chargeback = chargeback_counts.get('true', 0)
+    chargeback_percent = (total_chargeback / total_transactions) * 100
+    
+    # Visualizar os resultados em um gráfico de pizza
+    plt.figure(figsize=(6, 6))
+    plt.pie([total_chargeback, total_transactions - total_chargeback], labels=['Com Chargeback', 'Sem Chargeback'], autopct='%1.1f%%', colors=['skyblue', 'lightgreen'])
+    plt.title('Percentual de Transações com Chargeback')
+    plt.show()
+else:
+    print("Não há transações com chargeback.")
+
+# Calcular a média dos valores das transações
+transaction_amounts = df['transaction_amount']
+mean_transaction_amount = round(df['transaction_amount'].mean(), 2)
+
+print("Média dos Valores das Transações:", mean_transaction_amount)
